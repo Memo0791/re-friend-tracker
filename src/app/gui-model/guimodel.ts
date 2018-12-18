0da976ce0303b58b2709d29eb9b0df1c6ec29475
MehmetPekerman <mpekerman@yahoo.com>
2018-12-18 08:39:41 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Peters Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -27,6 +27,21 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+						¨{ 
+    "id":   "group", 
+    "type": "autocomplete", 
+    "name": "Group", 
+    "url": "/group", 
+    "form": "GroupForm", 
+    "width": 2 
+}, 
+						{ 
+	"id": "nickname", 
+    "type": "text", 
+    "name": "Nickname", 
+    "width": 2, 
+    "required": true 
+}, 
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -60,7 +75,33 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
+						{ 
+    "id": "GroupForm", 
+    "title": "Group", 
+    "url": "/group", 
+    "formFieldList": [ 
+        { 
+            "id": "name", 
+            "type": "text", 
+            "name": "GroupName", 
+            "width": 2, 
+            "required": true 
+        }, 
+        { 
+            "type": "deleteButton", 
+            "name": "Delete" 
+        }, 
+        { 
+            "type": "cancelButton", 
+            "name": "Cancel" 
+        }, 
+        { 
+            "type": "okButton", 
+            "name": "Ok" 
+        } 
+    ] 
+} 
                     ]
                 },
                 {
@@ -120,6 +161,7 @@ export class GuiModel {
                 },
             ],
             "pageList": [
+			
                 {
                     "id": "mainmenu",
                     "name": "MainMenu",
@@ -138,6 +180,41 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+									{ 
+    "type": "button", 
+    "name": "Groups", 
+    "icon": "fa-weixin", 
+    "color": "wisteria", 
+    "page": "groupspage", 
+}, 
+
+						{ 
+    "id": "groupspage", 
+    "elementList": [ 
+        { 
+            "type": "backbutton", 
+        }, 
+        { 
+            "type": "newButton", 
+            "name": "NewGroup", 
+            "icon": "fa-weixin", 
+            "color": "green", 
+            "form": { 
+                "form": "GroupForm" 
+            } 
+			{ 
+ "type": "list", 
+ "icon": "fa-weixin", 
+ "color": "wisteria", 
+ "search": true, 
+ "url": "/group", 
+ "form": { 
+ "form": "GroupForm" 
+ } 
+        } 
+    ] 
+} 
+			
                     ]
                 },
                 {
